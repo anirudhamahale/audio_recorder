6591ca4970e58cc6bf3b7ea15e5e4489c8faad6b
Anirudha Mahale <anirudhamahale@users.noreply.github.com>
2020-10-12 13:18:34 +0530

    Update AudioRecorderPlugin.m

--- ios/Classes/AudioRecorderPlugin.m
+++ ios/Classes/AudioRecorderPlugin.m
@@ -1,5 +1,12 @@
 #import "AudioRecorderPlugin.h"
+#if __has_include(<audio_recorder/audio_recorder-Swift.h>)
 #import <audio_recorder/audio_recorder-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "audio_recorder-Swift.h"
+#endif
 
 @implementation AudioRecorderPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
